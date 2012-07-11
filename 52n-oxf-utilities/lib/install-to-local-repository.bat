b03f897f44cfa28e8be1c8e744eb033a7e507a6f
ridoo <h.bredel@52north.org>
2012-07-11 14:43:16 +0000

    delete old libs (kept from pre-Maven Era)

--- 52n-oxf-utilities/lib/install-to-local-repository.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-mvn install:install-file -DgroupId=jfreechart -DartifactId=jfreechart -Dversion=1.0.0 -Dpackaging=jar -Dfile=jfreechart.jar
-REM mvn install:install-file -DgroupId=units -DartifactId=units -Dversion=0.0.1 -Dpackaging=jar -Dfile=units-0.0.1.jar
--- 52n-oxf-utilities/lib/lib_readme.txt
+++
@@ -1,4 +0,0 @@
-JFreeChart 1.0.0:
-
-jcommon-1.0.0.jar
-jfreechart-1.0.0.jar
