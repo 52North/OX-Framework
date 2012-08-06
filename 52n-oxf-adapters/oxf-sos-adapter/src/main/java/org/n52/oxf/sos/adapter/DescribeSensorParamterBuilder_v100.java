b8282de766e4aeadb1c1ffcff203708146d16269
EFI-52n <e.fiedler@52north.org>
2012-08-06 11:53:12 +0000

    Assembling class for parameters of wrapped "DescribeSensor" operation from SOSWrapper.

---
+++ 52n-oxf-adapters/oxf-sos-adapter/src/main/java/org/n52/oxf/sos/adapter/DescribeSensorParamterBuilder_v100.java
@@ -0,0 +1,39 @@
+package org.n52.oxf.sos.adapter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class describes a set of mandatory parameters, which is necessary to call doDescribeSensor([...]) from SOSWrapper.
+ * 
+ * @author Eric
+ */
+public class DescribeSensorParamterBuilder_v100 {
+
+	public static final String OUTPUT_FORMAT_SENSORML = "text/xml;subtype=\"sensorML/1.0.0\""; // defined output format for sensorml
+	public static final String OUTPUT_FORMAT_TML = "text/xml;subtype=\"TML/1.0\""; // defined output format for tml
+	
+	private Map<String, String> parameters = new HashMap<String, String>(); // set of mandatory parameters
+	
+	/**
+	 * Assembles mandatory parameters from method parameter list.
+	 * 
+	 * @param sensorId
+	 * @param outputFormat
+	 */
+	public DescribeSensorParamterBuilder_v100(String sensorId, String outputFormat) throws IllegalArgumentException {
+		if (sensorId == null || outputFormat == null)
+			throw new IllegalArgumentException("The parameters \"sensorId\" and \"outputFormat\" are mandatory. They cannot be left empty!");
+		// TODO create constant in ISOSRequestBuilder?
+		parameters.put("sensorId", sensorId);
+		parameters.put(ISOSRequestBuilder.DESCRIBE_SENSOR_OUTPUT_FORMAT, outputFormat);
+	}
+
+	/**
+	 * @return set of parameters
+	 */
+	public Map<String, String> getParameters() {
+		return parameters;
+	}
+
+}
