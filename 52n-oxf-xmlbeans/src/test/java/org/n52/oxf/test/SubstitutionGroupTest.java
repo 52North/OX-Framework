59ea6235d17535534861493bc6eb07f22c76b709
ridoo <h.bredel@52north.org>
2012-08-15 15:06:41 +0000

    doing some reordering add test to play around (commented out)

--- 52n-oxf-xmlbeans/src/test/java/org/n52/oxf/test/SubstitutionGroupTest.java
+++ 52n-oxf-xmlbeans/src/test/java/org/n52/oxf/test/SubstitutionGroupTest.java
@@ -21,102 +21,146 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
  * visit the Free Software Foundation web page, http://www.fsf.org.
  */
+
 package org.n52.oxf.test;
 
-import junit.framework.TestCase;
-
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import net.opengis.gml.AbstractFeatureCollectionType;
 import net.opengis.gml.AbstractFeatureType;
 import net.opengis.gml.FeatureCollectionDocument;
+import net.opengis.gml.FeaturePropertyDocument;
 import net.opengis.gml.FeaturePropertyType;
 import net.opengis.gml.GridCoverageDocument;
 import net.opengis.gml.GridCoverageType;
 import net.opengis.sampling.x10.SamplingFeatureDocument;
 import net.opengis.sampling.x10.SamplingFeatureType;
 
+import org.apache.xmlbeans.SchemaType;
+import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlOptions;
+import org.junit.Before;
+import org.junit.Test;
 import org.n52.oxf.xmlbeans.tools.XMLBeansTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Test case for substitution group and the qualification of those using
- * {@link XMLBeansTools#qualifySubstitutionGroup(org.apache.xmlbeans.XmlObject, javax.xml.namespace.QName, org.apache.xmlbeans.SchemaType)}.
+ * {@link XMLBeansTools#qualifySubstitutionGroup(org.apache.xmlbeans.XmlObject, javax.xml.namespace.QName, org.apache.xmlbeans.SchemaType)}
+ * .
  * 
  * @author matthes rieke
- *
+ * 
  */
-public class SubstitutionGroupTest extends TestCase {
-	
-	private static final Logger logger = LoggerFactory.getLogger(SubstitutionGroupTest.class);
-	private static XmlOptions options;
-	
-	static {
-		options = new XmlOptions();
-		options.setSavePrettyPrint();
-	}
+public class SubstitutionGroupTest {
 
-	public void testUnresolvedSubstitution() {
-		FeatureCollectionDocument collectionDoc = FeatureCollectionDocument.Factory.newInstance();
-		AbstractFeatureCollectionType collection = collectionDoc.addNewFeatureCollection();
+    private static final Logger logger = LoggerFactory.getLogger(SubstitutionGroupTest.class);
+    private static XmlOptions options;
+    private FeatureCollectionDocument collectionDoc;
+    private AbstractFeatureCollectionType collection;
 
-		addExtensionMemberWithoutQualifying(collection);
-		
-		//results in invalid xml markup
-		logger.info(collectionDoc.xmlText(options));
-		
-		addExtensionMemberWithQualifying(collection);
-		
-		//results in  valid xml markup
-		logger.info(collectionDoc.xmlText(options));
-	}
+    static {
+        options = new XmlOptions();
+        options.setSavePrettyPrint();
+    }
 
-	private void addExtensionMemberWithoutQualifying(
-			AbstractFeatureCollectionType collection) {
-		SamplingFeatureType sampling = SamplingFeatureType.Factory.newInstance();
-		sampling.addNewDescription().setStringValue("testDescription");
+    @Before
+    public void setUp() throws Exception {
+        collectionDoc = FeatureCollectionDocument.Factory.newInstance();
+        collection = collectionDoc.addNewFeatureCollection();
+    }
 
-		FeaturePropertyType member = collection.addNewFeatureMember();
-		member.setFeature(sampling);
-		
-		assertTrue(member.getFeature() != null);
-	}
+    @Test
+    public void testAddingExtensionMemberWithoutQualifying() {
+        logger.info("NO QUALIFYING");
+        SamplingFeatureType sampling = SamplingFeatureType.Factory.newInstance();
+        sampling.addNewDescription().setStringValue("testDescription");
 
-	private void addExtensionMemberWithQualifying(AbstractFeatureCollectionType collection) {
-		SamplingFeatureType sampling = SamplingFeatureType.Factory.newInstance();
-		sampling.addNewDescription().setStringValue("testDescription");
+        FeaturePropertyType member = collection.addNewFeatureMember();
+        member.setFeature(sampling);
+        
+        // results in invalid xml markup
+        logger.info("\nINVALID collection: \n \n{}\n", collectionDoc.xmlText(options));
 
-		FeaturePropertyType member = collection.addNewFeatureMember();
-		AbstractFeatureType feature = member.addNewFeature();
-		feature.set(sampling);
-		
-		// result is the same - member.getFeature() returns null
-//		member.setFeature(sampling);
-		
-		XMLBeansTools.qualifySubstitutionGroup(member.getFeature(), SamplingFeatureDocument.type.getDocumentElementName(), SamplingFeatureType.type);
-		
-		assertTrue(member.getFeature() == null);
-	}
+        assertTrue(member.getFeature() != null);
+    }
 
-	public void testBuiltInSubstitution() {
-		FeatureCollectionDocument collectionDoc = FeatureCollectionDocument.Factory.newInstance();
-		AbstractFeatureCollectionType collection = collectionDoc.addNewFeatureCollection();
+    @Test
+    public void testAddingExtensionMemberViaXmlBeans() {
+        try {
+            logger.info("USE BUILT-IN XMLBEANS SUBSTITUTION");
+            SamplingFeatureDocument samplingDoc = SamplingFeatureDocument.Factory.newInstance();
+            SamplingFeatureType sampling = samplingDoc.addNewSamplingFeature();
+            sampling.addNewDescription().setStringValue("testDescription");
 
-		addBuiltInMember(collection);
-		
-		logger.info(collectionDoc.xmlText(options));
-	}
+            
+            FeaturePropertyDocument featureMemberDoc = FeaturePropertyDocument.Factory.newInstance();
+            FeaturePropertyType member = featureMemberDoc.addNewFeatureProperty();
+//            FeaturePropertyType member = collection.addNewFeatureMember();
+            member.setFeature(sampling);
+            
+            collection.addNewFeatureMember().set(featureMemberDoc);
+            
+            SchemaType type = SamplingFeatureDocument.type;
+            XmlObject substitute = member.getFeature().substitute(type.getDocumentElementName(), sampling.schemaType());
+            logger.info("substituted element: {}", substitute.xmlText(options));
+            
+            // results in invalid xml markup
+            logger.info("\nINVALID collection: \n \n{}\n", collectionDoc.xmlText(options));
 
-	private void addBuiltInMember(AbstractFeatureCollectionType collection) {
-		GridCoverageType gridCoverage = GridCoverageType.Factory.newInstance();
-		gridCoverage.addNewDescription().setStringValue("testCoverage");
+            assertTrue(member.getFeature() != null);
+        } catch (Exception e) {
+            logger.error("Could not substitute by XmlBeans' built-in substitution means", e);
+            fail();
+        }
+    }
+    
+    @Test
+    public void testAddingExtensionMemberWithQualifyingFromXmlBeansTools() {
+        logger.info("SUBSTITUTION VIA XMLBeansTools");
+        SamplingFeatureType sampling = SamplingFeatureType.Factory.newInstance();
+        sampling.addNewDescription().setStringValue("testDescription");
 
-		FeaturePropertyType member = collection.addNewFeatureMember();
-		member.setFeature(gridCoverage);
-		
-		XMLBeansTools.qualifySubstitutionGroup(member.getFeature(), GridCoverageDocument.type.getDocumentElementName(), GridCoverageType.type);
-		
-		assertTrue(member.getFeature() != null);
-	}
+        FeaturePropertyType member = collection.addNewFeatureMember();
+        AbstractFeatureType feature = member.addNewFeature();
+        feature.set(sampling);
+
+        // result is the same - member.getFeature() returns null
+        // member.setFeature(sampling);
+
+        XMLBeansTools.qualifySubstitutionGroup(member.getFeature(),
+                                               SamplingFeatureDocument.type.getDocumentElementName(),
+                                               SamplingFeatureType.type);
+
+        // results in valid xml markup
+        logger.info("\nVALID collection: \n \n{}\n", collectionDoc.xmlText(options));
+        assertTrue(member.getFeature() == null);
+    }
+
+//    @Test
+    public void testBuiltInSubstitution() {
+        logger.info("BUILT-IN MEMBER SUBSTITUTION");
+        FeatureCollectionDocument collectionDoc = FeatureCollectionDocument.Factory.newInstance();
+        AbstractFeatureCollectionType collection = collectionDoc.addNewFeatureCollection();
+
+        addBuiltInMember(collection);
+
+        logger.info("\nVALID collection: \n \n{}\n", collectionDoc.xmlText(options));
+    }
+
+    private void addBuiltInMember(AbstractFeatureCollectionType collection) {
+        GridCoverageType gridCoverage = GridCoverageType.Factory.newInstance();
+        gridCoverage.addNewDescription().setStringValue("testCoverage");
+
+        FeaturePropertyType member = collection.addNewFeatureMember();
+        member.setFeature(gridCoverage);
+
+        XMLBeansTools.qualifySubstitutionGroup(member.getFeature(),
+                                               GridCoverageDocument.type.getDocumentElementName(),
+                                               GridCoverageType.type);
+
+        assertTrue(member.getFeature() != null);
+    }
 
 }
