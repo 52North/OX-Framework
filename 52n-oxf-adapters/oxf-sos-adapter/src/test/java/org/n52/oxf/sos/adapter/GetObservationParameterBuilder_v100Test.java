2e6713a4bdac9f17037dd7fe5ca037bd529df519
EFI-52n <e.fiedler@52north.org>
2012-10-23 09:56:32 +0000

    updated old tests

--- 52n-oxf-adapters/oxf-sos-adapter/src/test/java/org/n52/oxf/sos/adapter/GetObservationParameterBuilder_v100Test.java
+++ 52n-oxf-adapters/oxf-sos-adapter/src/test/java/org/n52/oxf/sos/adapter/GetObservationParameterBuilder_v100Test.java
@@ -1,9 +1,9 @@
 package org.n52.oxf.sos.adapter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.util.HashMap;
-import java.util.Vector;
 
 import org.junit.Test;
 import org.n52.oxf.OXFException;
--- 52n-oxf-adapters/oxf-sos-adapter/src/test/java/org/n52/oxf/sos/adapter/RegisterSensorParameterBuilder_v100Test.java
+++ 52n-oxf-adapters/oxf-sos-adapter/src/test/java/org/n52/oxf/sos/adapter/RegisterSensorParameterBuilder_v100Test.java
@@ -1,14 +1,11 @@
 package org.n52.oxf.sos.adapter;
 
-import static org.n52.oxf.sos.adapter.ISOSRequestBuilder.REGISTER_SENSOR_ML_DOC_PARAMETER;
+import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import org.junit.Test;
-import org.n52.oxf.OXFException;
-
-import static org.n52.oxf.sos.adapter.ISOSRequestBuilder.*;
+import org.n52.oxf.sos.adapter.wrapper.builder.RegisterSensorParameterBuilder_v100;
 
 
 /**
@@ -23,80 +20,35 @@ public class RegisterSensorParameterBuilder_v100Test {
 	/**
 	 * Checks the behaviour on valid constructor parameters.
 	 */
-	// @Test TODO activate
-//	public void testValidConstructorParameters() {
-//		SensorDescriptionBuilder sensorDescription = new SensorDescriptionBuilder("sensorId", REGISTER_SENSOR_OBSERVATION_TYPE_MEASUREMENT);
-//		sensorDescription.setPosition("name", true, 0.1, 2.3);
-//		sensorDescription.setObservedProperty("observedProperty");
-//		sensorDescription.setUnitOfMeasurement("uom");
-//		String sensorML = sensorDescription.generateSensorDescription();
-//		
-//		ObservationTemplateBuilder templateBuilder = ObservationTemplateBuilder.createObservationTemplateBuilderForTypeCategory("codeSpace");
-//		String obsTemp = null;
-//		try {
-//			obsTemp = templateBuilder.generateObservationTemplate();
-//		} catch (OXFException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		new RegisterSensorParameterBuilder_v100(sensorML, obsTemp);
-//	}
+	@Test
+	public void testValidConstructorParameters() {
+		new RegisterSensorParameterBuilder_v100("", "");
+	}
 	
 	/**
 	 * Checks the behaviour on invalid constructor parameters.
 	 */
-	// @Test(expected = IllegalArgumentException.class) TODO activate
-//	public void testInvalidConstructorParameters() {
-//		SensorDescriptionBuilder sensorDescription = new SensorDescriptionBuilder("sensorId", REGISTER_SENSOR_OBSERVATION_TYPE_MEASUREMENT);
-//		sensorDescription.setPosition("name", true, 0.1, 2.3);
-//		sensorDescription.setObservedProperty("observedProperty");
-//		sensorDescription.setUnitOfMeasurement("uom");
-//		String sensorML = sensorDescription.generateSensorDescription();
-//		
-//		ObservationTemplateBuilder templateBuilder = ObservationTemplateBuilder.createObservationTemplateBuilderForTypeCategory("codeSpace");
-//		String obsTemp = null;
-//		try {
-//			obsTemp = templateBuilder.generateObservationTemplate();
-//		} catch (OXFException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		new RegisterSensorParameterBuilder_v100(null, null);
-//		new RegisterSensorParameterBuilder_v100(sensorML, null);
-//		new RegisterSensorParameterBuilder_v100(null, obsTemp);
-//	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidConstructorParameters() {
+		new RegisterSensorParameterBuilder_v100(null, null);
+		new RegisterSensorParameterBuilder_v100("", null);
+		new RegisterSensorParameterBuilder_v100(null, "");
+	}
 	
 	/**
 	 * Checks, whether the mandatory parameters were applied correctly.
 	 */
-	// @Test TODO activate
-//	public void testApplyingAndGettingMandatoryParameters() {
-//		SensorDescriptionBuilder sensorDescription = new SensorDescriptionBuilder("sensorId", REGISTER_SENSOR_OBSERVATION_TYPE_MEASUREMENT);
-//		sensorDescription.setPosition("name", true, 0.1, 2.3);
-//		sensorDescription.setObservedProperty("observedProperty");
-//		sensorDescription.setUnitOfMeasurement("uom");
-//		String sensorML = sensorDescription.generateSensorDescription();
-//		
-//		ObservationTemplateBuilder templateBuilder = ObservationTemplateBuilder.createObservationTemplateBuilderForTypeCategory("codeSpace");
-//		String obsTemp = null;
-//		try {
-//			obsTemp = templateBuilder.generateObservationTemplate();
-//		} catch (OXFException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		RegisterSensorParameterBuilder_v100 rspb = new RegisterSensorParameterBuilder_v100(sensorML, obsTemp);
-//		
-//		Map<String, String> hm = (HashMap<String, String>) rspb.getParameters();
-//		String parMan_01 = hm.get(REGISTER_SENSOR_ML_DOC_PARAMETER);
-//		String parMan_02 = hm.get(REGISTER_SENSOR_OBSERVATION_TEMPLATE);
-//		
-//		// TODO insert comparable data
-//		// assertEquals(, parMan_01);
-//		// assertEquals(, parMan_02);
-//		
-//		System.out.println(parMan_01);
-//		System.out.println(parMan_02);
-//	}
+	@Test
+	public void testApplyingAndGettingMandatoryParameters() {
+		RegisterSensorParameterBuilder_v100 dspb = new RegisterSensorParameterBuilder_v100
+				("sensorDescription", "observationTemplate");
+		
+		HashMap<String, String> hm = (HashMap<String, String>) dspb.getParameters();
+		String parMan_01 = hm.get(ISOSRequestBuilder.REGISTER_SENSOR_ML_DOC_PARAMETER);
+		String parMan_02 = hm.get(ISOSRequestBuilder.REGISTER_SENSOR_OBSERVATION_TEMPLATE);
+		
+		assertEquals("sensorDescription", parMan_01);
+		assertEquals("observationTemplate", parMan_02);
+	}
 
 }
