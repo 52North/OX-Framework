ea4f61a6545c7d7c2b6e0f04c6625de86b153986
ridoo <h.bredel@52north.org>
2012-11-14 14:16:52 +0000

    javadoc

--- 52n-oxf-core/oxf-common/src/main/java/org/n52/oxf/util/web/HttpClient.java
+++ 52n-oxf-core/oxf-common/src/main/java/org/n52/oxf/util/web/HttpClient.java
@@ -1,8 +1,6 @@
 
 package org.n52.oxf.util.web;
 
-import java.util.Map;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.entity.ContentType;
@@ -18,11 +16,13 @@ public interface HttpClient {
     /**
      * @param baseUri
      *        the target to send the GET request to.
-     * @param queryParameters 
-     * @return
+     * @param parameters
+     *        the request/query parameters.
+     * @return the response entity returned by the target.
      * @throws HttpClientException
+     *         if sending the request fails.
      */
-    public HttpEntity executeGet(String baseUri, Map<String, String> queryParameters) throws HttpClientException;
+    public HttpEntity executeGet(String baseUri, RequestParameters parameters) throws HttpClientException;
 
     /**
      * Sends the given payload as content-type text/xml to the determined URI.
@@ -52,8 +52,26 @@ public interface HttpClient {
      */
     public HttpEntity executePost(String uri, String payloadToSend, ContentType contentType) throws HttpClientException;
 
+    /**
+     * Sends the given payload to the determined URI. Refer to the <a
+     * href="http://hc.apache.org/httpcomponents-core-ga/httpcore/apidocs/index.html">HTTP components docs</a>
+     * to get more information which entity types are possible.
+     * 
+     * @param uri
+     *        the target to send the POST request to.
+     * @param payloadToSend
+     *        a more generic way to send arbitrary content.
+     * @return the response entity returned by the target.
+     * @throws HttpClientException
+     *         if sending the request fails.
+     */
     public HttpEntity executePost(String uri, HttpEntity payloadToSend) throws HttpClientException;
 
+    /**
+     * @param method
+     * @return
+     * @throws HttpClientException
+     */
     public HttpEntity executeMethod(HttpRequestBase method) throws HttpClientException;
 
 }
