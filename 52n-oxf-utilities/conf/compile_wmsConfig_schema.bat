47e932bda8f9fa48c7e6eec63371eb75f315e7fc
Henning Bredel <h.bredel@52north.org>
2013-03-20 16:47:35 +0100

    remove client and wms adapter (development discontinued)

--- 52n-oxf-utilities/conf/compile_wmsConfig_schema.bat
+++
@@ -1,1 +0,0 @@
-C:\Programme\Java\xmlbeans-2.1.0\bin\scomp -dl -src "D:\Eigene Dateien\_Job\OXFramework\oxf-utilities\conf\compiled_schemas" -d "D:\Eigene Dateien\_Job\OXFramework\oxf-utilities\conf\compiled_schemas\bin" -out "D:\Eigene Dateien\_Job\OXFramework\oxf-utilities\conf\compiled_schemas\jar" wmsConfig_schema.xsd
