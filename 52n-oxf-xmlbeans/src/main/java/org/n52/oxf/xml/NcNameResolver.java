f94d3d6a2e48913d6d6212534c8c4d792a31526e
Henning Bredel <h.bredel@52north.org>
2013-04-11 11:46:57 +0200

    clean up pom.xml files fix license plugin configuration

--- 52n-oxf-xmlbeans/src/main/java/org/n52/oxf/xml/NcNameResolver.java
+++ 52n-oxf-xmlbeans/src/main/java/org/n52/oxf/xml/NcNameResolver.java
@@ -1,36 +1,11 @@
-/**
- * ﻿Copyright (C) 2012
- * by 52 North Initiative for Geospatial Open Source Software GmbH
- *
- * Contact: Andreas Wytzisk
- * 52 North Initiative for Geospatial Open Source Software GmbH
- * Martin-Luther-King-Weg 24
- * 48155 Muenster, Germany
- * info@52north.org
- *
- * This program is free software; you can redistribute and/or modify it under
- * the terms of the GNU General Public License version 2 as published by the
- * Free Software Foundation.
- *
- * This program is distributed WITHOUT ANY WARRANTY; even without the implied
- * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program (see gnu-gpl v2.txt). If not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
- * visit the Free Software Foundation web page, http://www.fsf.org.
- */
 package org.n52.oxf.xml;
 
 /**
  * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2006.<br />
  * Licensed under the Aduna BSD-style license.<br />
- * Author not defined in source!<br />
+
  * <ol><li><a href="http://www.java2s.com/Code/Java/XML/CheckswhetherthesuppliedStringisanNCNameNamespaceClassifiedName.htm">Source1</a></li>
  * <li><a href="http://www.openrdf.org/doc/sesame2/api/index.html?info/aduna/xml/XMLUtil.html">Source2</a></li></ol>
- * 
- * @author <a href="mailto:e.h.juerrens@52north.org">Eike Hinderk J&uuml;rrens</a>
  */
 public class NcNameResolver {
 
