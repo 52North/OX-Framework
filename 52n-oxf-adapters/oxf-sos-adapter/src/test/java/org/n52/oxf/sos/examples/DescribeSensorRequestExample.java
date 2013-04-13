74d1460e27261bea0b3d16cdbb3bd6ea87e3ce16
Henning Bredel <h.bredel@52north.org>
2013-04-13 14:36:09 +0200

    add GetObservationRequestExample and refactor examples

--- 52n-oxf-adapters/oxf-sos-adapter/src/test/java/org/n52/oxf/sos/examples/DescribeSensorRequestExample.java
+++ 52n-oxf-adapters/oxf-sos-adapter/src/test/java/org/n52/oxf/sos/examples/DescribeSensorRequestExample.java
@@ -1,26 +1,20 @@
 package org.n52.oxf.sos.examples;
 
-import static org.junit.Assert.fail;
 import static org.n52.oxf.sos.adapter.ISOSRequestBuilder.DESCRIBE_SENSOR_OUTPUT_FORMAT;
-import static org.n52.oxf.sos.adapter.ISOSRequestBuilder.DESCRIBE_SENSOR_PROCEDURE_DESCRIPTION_FORMAT;
 import static org.n52.oxf.sos.adapter.ISOSRequestBuilder.DESCRIBE_SENSOR_PROCEDURE_PARAMETER;
 import static org.n52.oxf.sos.adapter.ISOSRequestBuilder.DESCRIBE_SENSOR_SERVICE_PARAMETER;
 import static org.n52.oxf.sos.adapter.ISOSRequestBuilder.DESCRIBE_SENSOR_VERSION_PARAMETER;
 import static org.n52.oxf.sos.adapter.SOSAdapter.DESCRIBE_SENSOR;
 
-import java.io.InputStream;
-
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.n52.oxf.OXFException;
-import org.n52.oxf.adapter.OperationResult;
 import org.n52.oxf.adapter.ParameterContainer;
-import org.n52.oxf.ows.ExceptionReport;
 import org.n52.oxf.ows.capabilities.Operation;
 
 @Ignore // comment out to run demo class via JUnit
-public class DescribeSensorRequestExample extends SosAdapterExample {
+public class DescribeSensorRequestExample extends SosAdapterRequestExample {
     
     @Before
     public void setUp() throws Exception {
@@ -29,29 +23,19 @@ public class DescribeSensorRequestExample extends SosAdapterExample {
 
     @Test
     public void describeSensor() {
-        try {
-            Operation operation = createDescribeSensorOperation();
-            OperationResult result = adapter.doOperation(operation, createParameterContainer());
-            InputStream responseStream = result.getIncomingResultAsStream();
-            parseResponse(responseStream);
-        }
-        catch (ExceptionReport e) {
-            fail("Error at remote:\n" + formatExceptionReport(e));
-        }
-        catch (OXFException e) {
-            fail("SOS operation failed: " + e.getMessage());
-        }
+        handleOperation(createDescribeSensorOperation());
     }
 
     private Operation createDescribeSensorOperation() {
         return new Operation(DESCRIBE_SENSOR, getServiceGETUrl(), getServicePOSTUrl());
     }
 
-    private ParameterContainer createParameterContainer() throws OXFException {
+    @Override
+    protected ParameterContainer createParameterContainer() throws OXFException {
         ParameterContainer parameters = new ParameterContainer();
         parameters.addParameterShell(DESCRIBE_SENSOR_SERVICE_PARAMETER, "SOS");
         parameters.addParameterShell(DESCRIBE_SENSOR_VERSION_PARAMETER, "1.0.0");
-        parameters.addParameterShell(DESCRIBE_SENSOR_PROCEDURE_DESCRIPTION_FORMAT, DESCRIBE_SENSOR_OUTPUT_FORMAT);
+        parameters.addParameterShell(DESCRIBE_SENSOR_OUTPUT_FORMAT, "text/xml;subtype=\"sensorML/1.0.1\"");
         parameters.addParameterShell(DESCRIBE_SENSOR_PROCEDURE_PARAMETER, "Wasserstand-Ledasperrwerk_Up_3880050");
         return parameters;
     }
--- 52n-oxf-adapters/oxf-sos-adapter/src/test/java/org/n52/oxf/sos/examples/GetCapabilitiesRequestExample.java
+++ 52n-oxf-adapters/oxf-sos-adapter/src/test/java/org/n52/oxf/sos/examples/GetCapabilitiesRequestExample.java
@@ -1,23 +1,18 @@
 package org.n52.oxf.sos.examples;
 
-import static org.junit.Assert.fail;
 import static org.n52.oxf.sos.adapter.ISOSRequestBuilder.GET_CAPABILITIES_ACCEPT_VERSIONS_PARAMETER;
 import static org.n52.oxf.sos.adapter.ISOSRequestBuilder.GET_CAPABILITIES_SERVICE_PARAMETER;
 import static org.n52.oxf.sos.adapter.SOSAdapter.GET_CAPABILITIES;
-
-import java.io.InputStream;
 
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.n52.oxf.OXFException;
-import org.n52.oxf.adapter.OperationResult;
 import org.n52.oxf.adapter.ParameterContainer;
-import org.n52.oxf.ows.ExceptionReport;
 import org.n52.oxf.ows.capabilities.Operation;
 
 @Ignore // comment out to run demo class via JUnit
-public class GetCapabilitiesRequestExample extends SosAdapterExample {
+public class GetCapabilitiesRequestExample extends SosAdapterRequestExample {
     
     @Before
     public void setUp() throws Exception {
@@ -27,25 +22,15 @@ public class GetCapabilitiesRequestExample extends SosAdapterExample {
     @Test
     @Ignore // for demonstration
     public void getCapabilities() {
-        try {
-            Operation operation = createGetCapabilitiesOperation();
-            OperationResult result = adapter.doOperation(operation, createParameterContainer());
-            InputStream responseStream = result.getIncomingResultAsStream();
-            parseResponse(responseStream);
-        }
-        catch (ExceptionReport e) {
-            fail("Error at remote:\n" + formatExceptionReport(e));
-        }
-        catch (OXFException e) {
-            fail("SOS operation failed: " + e.getMessage());
-        }
+        handleOperation(createGetCapabilitiesOperation());
     }
 
     private Operation createGetCapabilitiesOperation() {
         return new Operation(GET_CAPABILITIES, getServiceGETUrl(), getServicePOSTUrl());
     }
 
-    private ParameterContainer createParameterContainer() throws OXFException {
+    @Override
+    protected ParameterContainer createParameterContainer() throws OXFException {
         ParameterContainer parameters = new ParameterContainer();
         parameters.addParameterShell(GET_CAPABILITIES_SERVICE_PARAMETER, "SOS");
         parameters.addParameterShell(GET_CAPABILITIES_ACCEPT_VERSIONS_PARAMETER, "1.0.0");
